779e8763fb7da0cad8157c5ddcb4bd84570a190d
Dhanasekaran <2001.dhanasekaran@gmail.com>
2024-10-30 13:08:34 +0530

    files added

---
+++ src/component/task.js
@@ -0,0 +1,16 @@
+import './task.css';
+function Display(){
+    return(
+        <>
+        <div className='container'>
+            <h1 className='head'>YOU CAN</h1>
+            <h1 className='head'>HIRE FREELANCER</h1>
+            <h1 className='head'>HERE</h1>
+            <p className='text'>It is a long established fact that a reader will be distracted by the readable content of a page</p>
+            <button className='about'>About us</button>
+            <button className='get'>Get A Quote</button>
+        </div>
+        </>
+    )
+}
+export default Display;
